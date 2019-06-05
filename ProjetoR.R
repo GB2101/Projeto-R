75ef4b05b381788066d9c948c2c986d12f6ed658
Gabriel Braz Cavalcante Silva <gbcs@ug3c07.windows.cin.ufpe.br>
2019-06-05 16:04:55 -0300

    06/05/2019 - 16:04

--- ProjetoR.R
+++ ProjetoR.R
@@ -10,6 +10,35 @@ print(got)
 
 #Segunda e Terceira Questão:
 result2 = moda(got$Nota)
-result3 = moda(got$Audiencia.Em.milhoes.)
 print(result2)
+
+#Terceira Questão:
+result3 = moda(got$Audiencia.Em.milhoes.)
 print(result3)
+
+#Quarta Questâo:
+got = read.csv("planilha_got.csv")
+nome = c()
+for (i in 1:length(got$Nota)){
+  if (got$Nota[i] >= 9){
+    nome = c(nome, toString(got$Episodio[i]))
+  }
+}
+
+#Quinta Questão:
+got = read.csv("planilha_got.csv")
+len = table(got$Temporada)
+t = 1
+x = 1
+for(i in 1:(t-1)){
+  x = x + as.numeric(len[i])
+}
+
+print(x)
+
+
+
+
+
+
+
