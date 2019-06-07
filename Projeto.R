a0f5b8e85c5248e9045dfd159c2dada6a2105458
Eduardo Guimaraes Medeiros <egm3@ug3c03.windows.cin.ufpe.br>
2019-06-07 11:04:57 -0300

    06/07/2019 - 11:04

--- Projeto.R
+++ Projeto.R
@@ -4,15 +4,16 @@
 
 got = read.csv("planilha_got.csv")
 
-moda = function(nota){
-  aux = as.numeric(names(which.max(table(nota))))
-  return(aux)
-}
-
 #Primeira Questão:
 print(got)
 
 #Segunda e Terceira Questão:
+#  Utilizamos a função table() para calcular a moda, tal função retorna a frequência de cada elemento do vetor,
+# depois obtemos o elemento de maior frequência.
+moda = function(nota){
+  aux = as.numeric(names(which.max(table(nota))))
+  return(aux)
+}
 result1 = mean(got$Nota)
 result2 = sd(got$Nota)
 result3 = moda(got$Nota)
@@ -21,6 +22,7 @@ print(result2)
 print(result3)
 
 #Terceira Questão:
+# Todas as funções já estavam implementadas na linguagem R
 result1 = mean(got$Audiencia.Em.milhoes.)
 result2 = sd(got$Audiencia.Em.milhoes.)
 result3 = median(got$Audiencia.Em.milhoes.)
@@ -29,6 +31,7 @@ print(result2)
 print(result3)
 
 #Quarta Questâo:
+# Itera pelo vetor de notas, e armazena o nome dos episódios de nota >= a 9
 nota_9 = function(){
   nome = c()
   for (i in 1:length(got$Nota)){
@@ -42,6 +45,9 @@ res = nota_9()
 print(res)
 
 #Quinta Questão:
+# A função minimax() encontra onde começa e termina a temporada t; e retorna o episódio de menor e maior nota da temporada.
+# A função notas() utiliza a função minimax() para todas as temporadas, armazenando em um dataframe os episódios e as notas.
+
 b = length(table(got$Temporada))
 minimax = function(t){
   len = table(got$Temporada)
@@ -83,6 +89,9 @@ res = notas()
 print(res)
 
 #Sexta Questão:
+# Equanto iteramos pelo vetor de temporadas, armazenamos os elementos de cada temporada.
+# Quando mudamos de temporada, calculamos o desvio padrão da temporada anterior e o armazenamos em um vetor.
+# Por fim retornamos a temporada de menor desvio padrão.
 desivo_padrao = function(){
   c = length(got$Temporada)
   t = 1
@@ -105,6 +114,7 @@ print(res)
 
 
 #Sétima Questão
+# Itera pelo vetor de personagens e armazena os episódios em que ele aparece.
 personagem = function(nome){
   c = length(got$Temporada)
   m = c()
@@ -124,6 +134,9 @@ print(res)
 
 
 #Oitava Questão:
+# Primeiramente, encontramos os episódios em que a temporada t começa e termina.
+# Então armazenamos todos os personagens de cada episódio em um vetor. Em seguida usamos a função table() para termos
+# a frequência de cada personagem. Por fim, retornamos os personagens que possuem frequência = 1.
 unico_episodio = function(t){
   len = table(got$Temporada)
   s = 1
@@ -155,6 +168,9 @@ print(res)
 
 
 #Nona Questão:
+# De início, calculamos a frequência do personagem em cada temporada, para isso utilizamos um metódo
+# semelhante ao da sexta questão. Dessa forma, utilizamos um inteiro, indicando a temporada, para  
+# cada episódio que o personagem aparece. Por fim, passamos esse vetor para o histograma.
 histograma = function(nome) {
   c = length(got$Temporada)
   t = 1
@@ -173,7 +189,7 @@ histograma = function(nome) {
     }
   }
   brk = c(0, 1, 2, 3, 4, 5, 6, 7, 8)
-  color = c("blue", "gray")
+  color = c("hot pink","red","orange","yellow","green", "turquoise", "blue", "purple")
   xl = "Temporada"
   yl = "Ocorrência"
   graph = hist(qq, main = nome, xlab = xl, ylab = yl, xlim = c(0, 9), ylim = c(0, 10), right = TRUE, breaks = brk, col = color, border = "black")
@@ -181,5 +197,5 @@ histograma = function(nome) {
 #Cersei Lannister(Lena Headey)
 #Bran Stark(Isaac Hempstead)
 #"Eddard 'Ned' Stark(Sean Bean)"
-nome = "Bran Stark(Isaac Hempstead)"
+nome = "Cersei Lannister(Lena Headey)"
 histograma(nome)
